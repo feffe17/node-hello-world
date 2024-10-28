796b00882b9781eb301b4de2f4c785014503022f
feffe1702 <fpm.asd@gmail.com>
2024-10-28 16:20:52 +0100

    [DONE] complete exercise

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 console.log("hello node js");
+
+const pin = process.env.PIN
+
+console.log(pin);
+
+const user = process.argv[2]
+
+console.log(`L'user è ${user}`);
+
+if (pin == 666 && user == "admin") {
+    console.log(`Welcome ${user}`);
+} else {
+    console.log("Access restricted");
+    
+}
+
